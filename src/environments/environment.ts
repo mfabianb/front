32bf5e10f3b5c4798e486438569ee5b3bee3c3c5
mfabianb <mfabianb08@gmail.com>
2021-11-29 01:08:21 -0600

    Environment production true

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   baseUrl: 'http://20.85.212.162:8081/crud/',
 
   msPath: {
